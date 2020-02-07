384b1c1567ff77bd63e1a7612d44748fbfe5e057
praveen264 <pstraveen264@gmail.com>
2020-02-07 09:57:41 +0530

    ok

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-var express=require('express');
-var app=express();
-
+var express = require('express');
+var app = express();
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+
+  res.render('index2.html');});
 app.listen(process.env.PORT||8080);
